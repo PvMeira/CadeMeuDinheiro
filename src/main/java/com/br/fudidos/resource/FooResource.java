23744c5b36b0b5af296fc12779ba8d9eae26e63d
Pedro Vitor Meira <pv.ciria@gmail.com>
2017-11-04 21:38:23 -0200

    Adding initial structure, and example

---
+++ src/main/java/com/br/fudidos/resource/FooResource.java
@@ -0,0 +1,11 @@
+package com.br.fudidos.resource;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by pvmeira on 04/11/2017.
+ */
+@RestController
+public class FooResource {
+    
+}
