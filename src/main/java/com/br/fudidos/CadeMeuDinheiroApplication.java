b7bd4dd55571460a760aeeb34256f6b52cd0d668
Pedro Vitor Meira <pv.ciria@gmail.com>
2017-11-04 21:27:42 -0200

    First commit

---
+++ src/main/java/com/br/fudidos/CadeMeuDinheiroApplication.java
@@ -0,0 +1,12 @@
+package com.br.fudidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadeMeuDinheiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadeMeuDinheiroApplication.class, args);
+	}
+}
